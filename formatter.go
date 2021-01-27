e0045a4a6bc44457d3b549e5d2c421ebd1700959
Ilya Beltyukov <968597@gmail.com>
2021-01-27 16:03:21 +0300

    init

---
+++ formatter.go
@@ -0,0 +1,39 @@
+package log
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"time"
+)
+
+type TextFormatter struct {
+	TimeFormat string
+}
+
+func (f *TextFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	var b *bytes.Buffer
+
+	if entry.Buffer != nil {
+		b = entry.Buffer
+	} else {
+		b = &bytes.Buffer{}
+	}
+
+	b.WriteString(fmt.Sprintf("[%s]: ", entry.Level.String()))
+
+	if f.TimeFormat == "" {
+		f.TimeFormat = time.RFC3339
+	}
+	b.WriteString(entry.Time.Format(f.TimeFormat))
+
+	if entry.Message != "" {
+		b.WriteString(fmt.Sprintf(": %s", entry.Message))
+	}
+
+	for key, val := range entry.Data {
+		b.WriteString(fmt.Sprintf(" (%s: %v)", key, val))
+	}
+	b.WriteByte('\n')
+	return b.Bytes(), nil
+}
